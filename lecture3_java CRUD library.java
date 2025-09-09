e2bc506976322fe48c6b2587486c4e07f490c5c2
dahlaetheron <dahlaetheron@gmail.com>
2025-09-09 21:24:09 +0900

    Update lecture3_java CRUD library.java

--- lecture3_java CRUD library.java
+++ lecture3_java CRUD library.java
@@ -17,10 +17,10 @@ public class BookHandler implements RequestHandler<Map<String, String>, String> 
         String title = event.get("title");
 
         if ("create".equals(action)) {
-            table.putItem(new Item().withPrimaryKey("bookId", id).withString("title", title));
+            table.putItem(new Item().withPrimaryKey("BookId", id).withString("title", title));
             return "Book created: " + title;
         } else if ("get".equals(action)) {
-            Item item = table.getItem(new GetItemSpec().withPrimaryKey("bookId", id));
+            Item item = table.getItem(new GetItemSpec().withPrimaryKey("BookId", id));
             return item == null ? "Book not found" : item.toJSON();
         }
         return "Unsupported action";
