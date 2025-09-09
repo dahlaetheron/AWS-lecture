b715be2c01b0c01d85dcfe860fc07ecf6be0fa6c
dahlaetheron <dahlaetheron@gmail.com>
2025-09-09 11:04:15 +0900

    Create lecture3_javaCRUDlibrary.java

---
+++ lecture_javaCRUDlibrary.java
@@ -0,0 +1,28 @@
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.dynamodbv2.*;
+import com.amazonaws.services.dynamodbv2.document.*;
+import com.amazonaws.services.dynamodbv2.document.spec.GetItemSpec;
+import java.util.Map;
+
+public class BookHandler implements RequestHandler<Map<String, String>, String> {
+
+    private final DynamoDB dynamoDB = new DynamoDB(AmazonDynamoDBClientBuilder.defaultClient());
+    private final Table table = dynamoDB.getTable("Books");
+
+    @Override
+    public String handleRequest(Map<String, String> event, Context context) {
+        String action = event.get("action");
+        String id = event.get("id");
+        String title = event.get("title");
+
+        if ("create".equals(action)) {
+            table.putItem(new Item().withPrimaryKey("bookId", id).withString("title", title));
+            return "Book created: " + title;
+        } else if ("get".equals(action)) {
+            Item item = table.getItem(new GetItemSpec().withPrimaryKey("bookId", id));
+            return item == null ? "Book not found" : item.toJSON();
+        }
+        return "Unsupported action";
+    }
+}
